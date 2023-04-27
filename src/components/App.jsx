a4a1660011d508f949079aac8d4a677859cf78a3
ShevchukOleh <shew4ukoleg12@gmail.com>
2023-04-27 18:54:08 +0300

    done 1-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
+import user from './Profile/user.json';
+import Profile from "./Profile/Profile";
+import Statistics from './Statistics/Statistics';
+import data from './Statistics/data.json';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div style={{ padding:20}}>
+      <Profile
+        key={user.tag}
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+      />
+
+      <Statistics
+        key={data.id}
+        title="Upload stats"
+        stats={data}
+      />
     </div>
   );
 };
